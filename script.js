59d4159210a66e558220efd652a84356d1f79cdb
SD-Album <sd-album@noreply.github.com>
2026-06-05 21:55:21 +0800

    SD相册 - 员工照片查询页面

---
+++ script.js
@@ -0,0 +1,219 @@
+// 自动从 images 文件夹生成
+const users = [
+  { jobNo: "520", wechat: "", phone: "", photo: "./images/520.jpg" },
+  { jobNo: "088", wechat: "", phone: "", photo: "./images/088.jpg" },
+  { jobNo: "605", wechat: "", phone: "", photo: "./images/605.jpg" },
+  { jobNo: "012", wechat: "", phone: "", photo: "./images/012.jpg" },
+  { jobNo: "369", wechat: "", phone: "", photo: "./images/369.jpg" },
+  { jobNo: "002", wechat: "", phone: "", photo: "./images/002.jpg" },
+  { jobNo: "006", wechat: "", phone: "", photo: "./images/006.jpg" },
+  { jobNo: "007", wechat: "", phone: "", photo: "./images/007.jpg" },
+  { jobNo: "008", wechat: "", phone: "", photo: "./images/008.jpg" },
+  { jobNo: "008_1", wechat: "", phone: "", photo: "./images/008_1.jpg" },
+  { jobNo: "009", wechat: "", phone: "", photo: "./images/009.png" },
+  { jobNo: "028", wechat: "", phone: "", photo: "./images/028.png" },
+  { jobNo: "61", wechat: "", phone: "", photo: "./images/61.jpg" },
+  { jobNo: "066", wechat: "", phone: "", photo: "./images/066.jpg" },
+  { jobNo: "077", wechat: "", phone: "", photo: "./images/077.jpg" },
+  { jobNo: "86", wechat: "", phone: "", photo: "./images/86.jpg" },
+  { jobNo: "099", wechat: "", phone: "", photo: "./images/099.jpg" },
+  { jobNo: "108", wechat: "", phone: "", photo: "./images/108.jpg" },
+  { jobNo: "122", wechat: "", phone: "", photo: "./images/122.png" },
+  { jobNo: "123", wechat: "", phone: "", photo: "./images/123.jpg" },
+  { jobNo: "126", wechat: "", phone: "", photo: "./images/126.jpg" },
+  { jobNo: "138", wechat: "", phone: "", photo: "./images/138.png" },
+  { jobNo: "139", wechat: "", phone: "", photo: "./images/139.jpg" },
+  { jobNo: "155", wechat: "", phone: "", photo: "./images/155.jpg" },
+  { jobNo: "166", wechat: "", phone: "", photo: "./images/166.jpg" },
+  { jobNo: "180", wechat: "", phone: "", photo: "./images/180.jpg" },
+  { jobNo: "188", wechat: "", phone: "", photo: "./images/188.jpg" },
+  { jobNo: "211", wechat: "", phone: "", photo: "./images/211.jpg" },
+  { jobNo: "222", wechat: "", phone: "", photo: "./images/222.jpg" },
+  { jobNo: "333", wechat: "", phone: "", photo: "./images/333.jpg" },
+  { jobNo: "500", wechat: "", phone: "", photo: "./images/500.png" },
+  { jobNo: "521", wechat: "", phone: "", photo: "./images/521.jpg" },
+  { jobNo: "555", wechat: "", phone: "", photo: "./images/555.jpg" },
+  { jobNo: "600", wechat: "", phone: "", photo: "./images/600.jpg" },
+  { jobNo: "603", wechat: "", phone: "", photo: "./images/603.jpg" },
+  { jobNo: "606", wechat: "", phone: "", photo: "./images/606.jpg" },
+  { jobNo: "608", wechat: "", phone: "", photo: "./images/608.jpg" },
+  { jobNo: "610", wechat: "", phone: "", photo: "./images/610.jpg" },
+  { jobNo: "611", wechat: "", phone: "", photo: "./images/611.jpg" },
+  { jobNo: "618", wechat: "", phone: "", photo: "./images/618.jpg" },
+  { jobNo: "619", wechat: "", phone: "", photo: "./images/619.jpg" },
+  { jobNo: "622", wechat: "", phone: "", photo: "./images/622.jpg" },
+  { jobNo: "626", wechat: "", phone: "", photo: "./images/626.jpg" },
+  { jobNo: "628", wechat: "", phone: "", photo: "./images/628.jpg" },
+  { jobNo: "633", wechat: "", phone: "", photo: "./images/633.jpg" },
+  { jobNo: "636", wechat: "", phone: "", photo: "./images/636.jpg" },
+  { jobNo: "655", wechat: "", phone: "", photo: "./images/655.jpg" },
+  { jobNo: "658", wechat: "", phone: "", photo: "./images/658.jpg" },
+  { jobNo: "660", wechat: "", phone: "", photo: "./images/660.jpg" },
+  { jobNo: "666", wechat: "", phone: "", photo: "./images/666.jpg" },
+  { jobNo: "668", wechat: "", phone: "", photo: "./images/668.jpg" },
+  { jobNo: "678", wechat: "", phone: "", photo: "./images/678.jpg" },
+  { jobNo: "686", wechat: "", phone: "", photo: "./images/686.jpg" },
+  { jobNo: "696", wechat: "", phone: "", photo: "./images/696.jpg" },
+  { jobNo: "700", wechat: "", phone: "", photo: "./images/700.jpg" },
+  { jobNo: "789", wechat: "", phone: "", photo: "./images/789.png" },
+  { jobNo: "800", wechat: "", phone: "", photo: "./images/800.jpg" },
+  { jobNo: "803", wechat: "", phone: "", photo: "./images/803.jpg" },
+  { jobNo: "808", wechat: "", phone: "", photo: "./images/808.jpg" },
+  { jobNo: "809", wechat: "", phone: "", photo: "./images/809.jpg" },
+  { jobNo: "816", wechat: "", phone: "", photo: "./images/816.jpg" },
+  { jobNo: "818", wechat: "", phone: "", photo: "./images/818.jpg" },
+  { jobNo: "822", wechat: "", phone: "", photo: "./images/822.jpg" },
+  { jobNo: "828", wechat: "", phone: "", photo: "./images/828.jpg" },
+  { jobNo: "833", wechat: "", phone: "", photo: "./images/833.jpg" },
+  { jobNo: "833_1", wechat: "", phone: "", photo: "./images/833_1.jpg" },
+  { jobNo: "858", wechat: "", phone: "", photo: "./images/858.jpg" },
+  { jobNo: "866", wechat: "", phone: "", photo: "./images/866.jpg" },
+  { jobNo: "868", wechat: "", phone: "", photo: "./images/868.jpg" },
+  { jobNo: "877", wechat: "", phone: "", photo: "./images/877.jpg" },
+  { jobNo: "880", wechat: "", phone: "", photo: "./images/880.jpg" },
+  { jobNo: "888", wechat: "", phone: "", photo: "./images/888.jpg" },
+  { jobNo: "898", wechat: "", phone: "", photo: "./images/898.jpg" },
+  { jobNo: "899", wechat: "", phone: "", photo: "./images/899.jpg" },
+  { jobNo: "899_1", wechat: "", phone: "", photo: "./images/899_1.jpg" },
+  { jobNo: "900", wechat: "", phone: "", photo: "./images/900.jpg" },
+  { jobNo: "0905", wechat: "", phone: "", photo: "./images/0905.png" },
+  { jobNo: "906", wechat: "", phone: "", photo: "./images/906.jpg" },
+  { jobNo: "908", wechat: "", phone: "", photo: "./images/908.jpg" },
+  { jobNo: "911", wechat: "", phone: "", photo: "./images/911.jpg" },
+  { jobNo: "913", wechat: "", phone: "", photo: "./images/913.jpg" },
+  { jobNo: "916", wechat: "", phone: "", photo: "./images/916.jpg" },
+  { jobNo: "918", wechat: "", phone: "", photo: "./images/918.jpg" },
+  { jobNo: "919", wechat: "", phone: "", photo: "./images/919.png" },
+  { jobNo: "933", wechat: "", phone: "", photo: "./images/933.jpg" },
+  { jobNo: "938", wechat: "", phone: "", photo: "./images/938.jpg" },
+  { jobNo: "966", wechat: "", phone: "", photo: "./images/966.jpg" },
+  { jobNo: "985", wechat: "", phone: "", photo: "./images/985.jpg" },
+  { jobNo: "986", wechat: "", phone: "", photo: "./images/986.png" },
+  { jobNo: "988", wechat: "", phone: "", photo: "./images/988.jpg" },
+  { jobNo: "995", wechat: "", phone: "", photo: "./images/995.jpg" },
+  { jobNo: "1001", wechat: "", phone: "", photo: "./images/1001.jpg" },
+  { jobNo: "A001-3", wechat: "", phone: "", photo: "./images/A001-3.jpg" },
+  { jobNo: "A001-6", wechat: "", phone: "", photo: "./images/A001-6.jpg" },
+  { jobNo: "A001-23", wechat: "", phone: "", photo: "./images/A001-23.jpg" },
+  { jobNo: "A001-25", wechat: "", phone: "", photo: "./images/A001-25.jpg" },
+  { jobNo: "A001-29", wechat: "", phone: "", photo: "./images/A001-29.jpg" },
+  { jobNo: "A001-35", wechat: "", phone: "", photo: "./images/A001-35.jpg" },
+  { jobNo: "A001-59", wechat: "", phone: "", photo: "./images/A001-59.jpg" },
+  { jobNo: "A001-60", wechat: "", phone: "", photo: "./images/A001-60.jpg" },
+  { jobNo: "A001-67", wechat: "", phone: "", photo: "./images/A001-67.jpg" },
+  { jobNo: "A001-72", wechat: "", phone: "", photo: "./images/A001-72.jpg" },
+  { jobNo: "A001-75", wechat: "", phone: "", photo: "./images/A001-75.jpg" },
+  { jobNo: "A001-78", wechat: "", phone: "", photo: "./images/A001-78.jpg" },
+  { jobNo: "A001-83", wechat: "", phone: "", photo: "./images/A001-83.png" },
+  { jobNo: "A001-102", wechat: "", phone: "", photo: "./images/A001-102.jpg" },
+  { jobNo: "微信图片_20260605155323_386_40", wechat: "", phone: "", photo: "./images/微信图片_20260605155323_386_40.jpg" }
+];
+
+let currentUser = null;
+
+function init() {
+  showUsers(users);
+  document.getElementById("jobInput").addEventListener("input", function() {
+    filterUsers(this.value);
+  });
+}
+
+function showUsers(list) {
+  const photoGrid = document.getElementById("photoGrid");
+  const photoTitle = document.getElementById("photoTitle");
+  photoTitle.innerText = "全部员工（共 " + list.length + " 人）";
+  photoGrid.innerHTML = "";
+
+  if (list.length === 0) {
+    photoTitle.innerText = "没有找到该工号";
+    return;
+  }
+
+  list.forEach(function(user) {
+    const card = document.createElement("div");
+    card.className = "photo-card-item";
+
+    const img = document.createElement("img");
+    img.src = user.photo;
+    img.alt = user.jobNo;
+    img.onclick = function() {
+      openViewer(user.photo);
+    };
+
+    const label = document.createElement("div");
+    label.className = "photo-label";
+    label.innerText = user.jobNo;
+
+    card.appendChild(img);
+    card.appendChild(label);
+    card.onclick = function(e) {
+      if (e.target.tagName !== "IMG") {
+        selectUser(user);
+      }
+    };
+
+    photoGrid.appendChild(card);
+  });
+}
+
+function filterUsers(keyword) {
+  const kw = keyword.trim();
+
+  if (kw === "") {
+    showUsers(users);
+    return;
+  }
+
+  const matched = users.filter(function(user) {
+    return user.jobNo.toLowerCase().indexOf(kw.toLowerCase()) !== -1;
+  });
+
+  showUsers(matched);
+}
+
+function selectUser(user) {
+  currentUser = user;
+  document.getElementById("photoTitle").innerText = "已选中：" + user.jobNo;
+  document.getElementById("jobInput").value = user.jobNo;
+}
+
+function searchUser() {
+  const input = document.getElementById("jobInput");
+  filterUsers(input.value);
+}
+
+function copyText(text) {
+  navigator.clipboard.writeText(text).then(function() {
+    alert("复制成功：" + text);
+  }).catch(function() {
+    alert("复制失败，请手动复制：" + text);
+  });
+}
+
+function openViewer(photoUrl) {
+  const imageViewer = document.getElementById("imageViewer");
+  const viewerImg = document.getElementById("viewerImg");
+  viewerImg.src = photoUrl;
+  imageViewer.style.display = "flex";
+}
+
+function closeViewer() {
+  const imageViewer = document.getElementById("imageViewer");
+  const viewerImg = document.getElementById("viewerImg");
+  imageViewer.style.display = "none";
+  viewerImg.src = "";
+}
+
+function toggleSchedule() {
+  const body = document.getElementById("scheduleBody");
+  const arrow = document.getElementById("scheduleArrow");
+  if (body.style.display === "none") {
+    body.style.display = "block";
+    arrow.classList.add("open");
+  } else {
+    body.style.display = "none";
+    arrow.classList.remove("open");
+  }
+}
+
+init();
