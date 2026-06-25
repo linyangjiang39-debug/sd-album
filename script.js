f0283a6fe8e20e6f2d5195da7068a438251ecd6f
J-droid-afk <linyangjiang39@gmail.com>
2026-06-25 16:05:16 +0800

    更新员工数据 - 2026/6/25 16:05:14

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // 自动生成 - SD相册员工数据
 const users = [
   {
-    jobNo: "077",
-    wechat: "",
-    phone: "",
-    photo: "./images/077.webp"
-  },
-  {
     jobNo: "186",
     wechat: "",
     phone: "",
